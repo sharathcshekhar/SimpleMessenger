cbb6bfc23fb0198256c432cfd035fc01822c0a0b
Sharath <sharathcshekhar@gmail.com>
2014-02-22 20:29:44 -0500

    +gitignore, -bin, +minor changes

--- src/edu/buffalo/cse/cse486586/simplemessenger/MainActivity.java
+++ src/edu/buffalo/cse/cse486586/simplemessenger/MainActivity.java
@@ -1,3 +1,17 @@
+/*
+* Department of Computer Science, University at Buffalo
+*
+* CSE 586 Project - 1
+*
+* Author: Sharath Chandrashekhara - sc296@buffalo.edu
+* 
+* Date: 30th Jan, 2013
+* 
+* Application: SimpleMessenger
+* 
+* Description: SimpleMessenger is an Android App to chat between 2 devices.
+*
+*/
 package edu.buffalo.cse.cse486586.simplemessenger;
 
 import java.io.BufferedReader;
@@ -29,9 +43,6 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 
 	final static String TAG = "MESSENGER";
-	final static String SERVER_TAG = "SERVER";
-	final static String CLIENT_TAG = "CLIENT";
-	
 	InetAddress partnersIP; 
 	int partnersPort = 0; 
 	
@@ -40,29 +51,42 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		partnersPort = getPartnersPort();
+		/*
+		 * All communications between AVDs running on the same host
+		 * uses the IP 10.0.2.2
+		 */
 		try {
 			partnersIP = InetAddress.getByAddress(new byte[]{10, 0, 2, 2});
 		} catch (UnknownHostException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
+			Log.v(TAG, "Cannot resolve IP address");
+			return;
 		}
-		
+		//Set the header to the Text view as - "Incoming Messages"
 		TextView msgs = (TextView) findViewById(R.id.textView1);
 		String str = "<b> Incoming Messages </b>";
 		msgs.append(Html.fromHtml(str));
 		msgs.append("\n\n");
 		msgs.setBackgroundColor(Color.LTGRAY);
 		ServerSocket serverSocket = null;
+		/*
+		 * Server socket number is hardcoded to 10000. The Virtual Router has to be set up
+		 * to redirect the incoming connections to 10000
+		 */
+		
 		try {
 			serverSocket = new ServerSocket(10000);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			Log.v(TAG, "Unable to create server socket");
+			return;
 		}
+		//Start the server as AsyncTask
 		new ServerThread().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, serverSocket);
 
 		final EditText user_ip = (EditText) findViewById(R.id.editText1);
 		
+		//Register an Event handler for key pressed
 		user_ip.setOnKeyListener(new OnKeyListener() {
 			@Override
 			public boolean onKey(View v, int keyCode, KeyEvent event) {
@@ -71,6 +95,7 @@ public class MainActivity extends Activity {
 					String msg = user_ip.getText().toString() + '\n';
 					user_ip.setText("");
 					new ClientThread().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, msg);
+					// msg consumed
 					return true;
 				}
 				return false;
@@ -79,15 +104,21 @@ public class MainActivity extends Activity {
 	}
 
 	private int getPartnersPort() {
-		// TODO Auto-generated method stub
 		TelephonyManager tel = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
-		String devId = tel.getLine1Number().substring(tel.getLine1Number().length() - 4);
-		if (devId.equals("5554")) {
+		String phNumber = tel.getLine1Number().substring(tel.getLine1Number().length() - 4);
+		/*
+		 * The last 4 digits of the phNumber gives the console port of the AVD
+		 * AVD running on console port 5554 is set to redirect connections to port
+		 * 11108 to 10000 and AVD running on 5556 is set to redirect connections
+		 * to port 11112 to 10000.
+		 */
+		if (phNumber.equals("5554")) {
 			return 11112;
-		} else if (devId.equals("5556")) {
+		} else if (phNumber.equals("5556")) {
 			return 11108;
 		}
-		Log.v(TAG, devId);
+		Log.v(TAG, phNumber);
+		//invalid AVD
 		return 0;
 	}
 
@@ -104,7 +135,11 @@ public class MainActivity extends Activity {
 			super.onProgressUpdate(values);
 			TextView msgs = (TextView) findViewById(R.id.textView1);
 			msgs.setMovementMethod(new ScrollingMovementMethod());
-			msgs.append(values[0] + '\n');
+			if (!(values[0].trim().equals(""))) {
+				msgs.append(values[0] + '\n');
+			}
+			// These 2 APIs will scroll to the bottom of the textView when a incoming
+			// Messages is received
 			msgs.setMovementMethod(new ScrollingMovementMethod());
 			((ScrollView) findViewById(R.id.scrollView)).fullScroll(View.FOCUS_DOWN);
 			Log.v(TAG, values[0]);
@@ -112,7 +147,6 @@ public class MainActivity extends Activity {
 
 		@Override
 		protected Void doInBackground(ServerSocket... params) {
-			// TODO Auto-generated method stub
 			ServerSocket serverSocket = params[0];
 			while (true) {
 				try {
@@ -130,19 +164,10 @@ public class MainActivity extends Activity {
 					clientSocket.close();
 				} catch (Exception e) {
 					e.printStackTrace();
-					return null;
+					//Socket exception, try again
+					continue;
 				}
 			}
-		}
-
-		/** Called when the user clicks the Send button */
-		public void SendMessage(View view) {
-			// Do something in response to button
-			EditText text = (EditText) findViewById(R.id.editText1);
-			String text_str = text.getText().toString();
-			// Log.v(TAG, text_str);
-			text.setText("");
-			new ClientThread().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, text_str);
 		}
 	}
 
@@ -156,22 +181,19 @@ public class MainActivity extends Activity {
 				Log.v(TAG, "Connecting to server");
 				Socket clientSocket = new Socket(partnersIP, partnersPort);
 				Log.v(TAG, "Connected to Server");
-
 				toServer = new DataOutputStream(clientSocket.getOutputStream());
 				toServer.writeBytes(message + '\n');
 				clientSocket.close();
 			} catch (UnknownHostException e) {
 				e.printStackTrace();
+				Log.v(TAG, "Unable to resole host");
+				return null;
 			} catch (IOException e) {
 				e.printStackTrace();
+				Log.v(TAG, "Unable to communicate with the server");
+				return null;
 			}
-			publishProgress(message);
 			return null;
-		}
-
-		protected void onProgressUpdate(String... values) {
-			super.onProgressUpdate(values);
-			Log.v(TAG, values[0]);
 		}
 	}
 }
